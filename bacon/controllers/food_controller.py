28885c2f00c3ccc02203558598f03ef39a8206fd
lydiacox <coxlydia01@gmail.com>
2022-03-05 12:05:36 +1100

    update templates and tests

--- bacon/controllers/food_controller.py
+++ bacon/controllers/food_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from main import db
 from models.food import Food
 from schemas.food_schema import food_schema, multi_food_schema
@@ -6,27 +6,37 @@ from schemas.food_schema import food_schema, multi_food_schema
 food = Blueprint('food', __name__)
 
 @food.route('/')
-def hello_world():
+def home():
     """
     The homepage route. 
-    
-    This will later contain information about what classes are available to enroll in.
     '/' is the address here, which means it will be available from our host domain. 
     During production this is localhost:5000 or 127.0.0.1:5000
     """
-    return "Hello, world! Check this out!"
+    data = {
+        "page_title": "Eggs & Bacon Homepage",
+    }
+    return render_template("index.html", page_data=data)
 
 @food.route('/login/', methods=["GET"])
 def login():
-    return "The login page"
+    data = {
+        "page_title": "Eggs & Bacon | Login",
+    }
+    return render_template("login.html", page_data=data)
 
-@food.route('/signup/', methods=["POST"])
+@food.route('/signup/', methods=["GET"])
 def signup():
-    return "The signup page"
+    data = {
+        "page_title": "Eggs & Bacon | Sign Up",
+    }
+    return render_template("signup.html", page_data=data)
 
 @food.route('/cook/', methods=["GET"])
 def cook():
-    return "The cooking page"
+    data = {
+        "page_title": "Eggs & Bacon | Let's Cook!",
+    }
+    return render_template("cook.html", page_data=data)
 
 # @food.route("/foods/", methods=["GET"])
 # def get_foods():
--- bacon/tests/test_foods.py
+++ bacon/tests/test_foods.py
@@ -1,5 +1,35 @@
 import unittest
+from main import create_app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+os.environ["FLASK_ENV"]="testing"
 
 class TestFoods(unittest.TestCase):
-    def test_food_index(self):
-        pass
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_homepage_index(self):
+        response = self.client.get("/")
+        # data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        # self.assertIsInstance(data, list)
+
+    def test_login(self):
+        response = self.client.get("/login/")
+        # data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        # self.assertIsInstance(data, list)
+
+    def test_signup(self):
+        response = self.client.get("/signup/")
+        # data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        # self.assertIsInstance(data, list)
+
+    # def test_create_bad_course(self):
+    #     response = self.client.post("/courses/", json={"course_name": ""})
+    #     self.assertEqual(response.status_code, 400)
